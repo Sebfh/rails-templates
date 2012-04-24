f939edacf772e816dd89270ce414450f74c355a3
Sebastiaan Hoejenbos <tikkieseb@gmail.com>
2012-04-24 22:40:36 +0200

    added inital text to mssql-app.rb

--- mssql-app.rb
+++ mssql-app.rb
@@ -1,2 +1,9 @@
 # this will be my default template when creating a Rails app that runs on MS SQL2008 database
-# testing
+run "rm public/index.html"
+generate(:scaffold, "person name:string")
+route "root :to => 'people#index'"
+rake("db:migrate")
+ 
+git :init
+git :add => "."
+git :commit => "-a -m 'Initial commit'"
